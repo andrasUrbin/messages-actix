16050716a552f8649b9a85df8ab58f2a26071b17
Andras Urbin <urbinandras@gmail.com>
2020-08-01 17:54:10 +0200

    AppState modified

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,20 @@ extern crate actix_web;
 
 use actix_web::{middleware, web, App, HttpRequest, HttpServer, Result};
 use serde::Serialize;
+use std::cell::Cell;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::{Arc, Mutex};
+
+static SERVER_COUNTER: AtomicUsize = AtomicUsize::new(0);
 
 pub struct MessageApp {
     port: u16,
+}
+
+struct AppState {
+    server_id: usize,
+    request_count: Cell<usize>,
+    messages: Arc<Mutex<Vec<String>>>,
 }
 
 impl MessageApp {
@@ -14,9 +25,15 @@ impl MessageApp {
     }
 
     pub fn run(&self) -> std::io::Result<()> {
+        let messages = Arc::new(Mutex::new(vec![]));
         println!("Starting http server: 127.0.0.1:{}", self.port);
         HttpServer::new(move || {
             App::new()
+                .data(AppState {
+                    server_id: SERVER_COUNTER.fetch_add(1, Ordering::SeqCst),
+                    request_count: Cell::new(0),
+                    messages: messages.clone(),
+                })
                 .wrap(middleware::Logger::default())
                 .service(index)
         })
@@ -28,18 +45,20 @@ impl MessageApp {
 
 #[derive(Serialize)]
 struct IndexResponse {
-    message: String,
+    server_id: usize,
+    request_count: usize,
+    messages: Vec<String>,
 }
 
 #[get("/")]
-fn index(req: HttpRequest) -> Result<web::Json<IndexResponse>> {
-    let hello = req
-        .headers()
-        .get("hello")
-        .and_then(|v| v.to_str().ok())
-        .unwrap_or_else(|| "world");
+fn index(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
+    let request_count = state.request_count.get() + 1;
+    state.request_count.set(request_count);
+    let ms = state.messages.lock().unwrap();
 
     Ok(web::Json(IndexResponse {
-        message: hello.to_owned(),
+        server_id: state.server_id,
+        request_count,
+        messages: ms.clone(),
     }))
 }
